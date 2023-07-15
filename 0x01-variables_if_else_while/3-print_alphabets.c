16fe793ef66822d417e94c89521da9991265ef02
SotirUsama1 <sotirusama16@gmail.com>
2023-07-14 19:19:40 -0700

    task_3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,18 @@ int main(void)
 {
 	char ch = 'a';
 
-	while (ch <= 'Z')
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		++ch;
 	}
+	char ch = 'A';
+
+        while (ch <= 'Z')
+        {
+                putchar(ch);
+                ++ch;
+        }
 	putchar('\n');
 
 	return (0);
