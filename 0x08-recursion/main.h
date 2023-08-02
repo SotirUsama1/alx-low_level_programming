7bc71878fd931bd474c21995705928d9d44680e1
SotirUsama1 <sotirusama16@gmail.com>
2023-08-02 09:40:05 -0700

    task_0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,15 +3,7 @@
 
 #include<stdio.h>
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
 int _putchar(char c);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
+void _puts_recursion(char *s);
 
 #endif /* MAIN_H */
