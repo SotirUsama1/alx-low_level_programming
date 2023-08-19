c774bbed96cbc9387f89e6aa296326ec13aa5f9c
SotirUsama1 <sotirusama16@gmail.com>
2023-08-18 21:38:49 -0700

    task_10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,11 +14,11 @@ void print_triangles(int size)
 		_putchar('\n');
 	else
 	{
-		for (int i = 0 ; i <= size ; ++i)
+		for (int i = 0; i <= size; ++i)
 		{
-			for (int j = 0 ; j < size - i ; ++j)
+			for (int j = 0; j < size - i; ++j)
 				_putchar(' ');
-			for (int l = 0 ; l < i ; ++l)
+			for (int l = 0; l < i; ++l)
 				_putchar('#');
 			_putchar('\n');
 		}
