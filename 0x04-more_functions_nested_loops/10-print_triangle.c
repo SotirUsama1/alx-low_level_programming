fca8c21d0d48749862d2566cd20243ef0a1703fe
SotirUsama1 <sotirusama16@gmail.com>
2023-08-18 21:10:27 -0700

    task_10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - print triangles
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_triangles(int size)
+{
+	if (size <= 0)
+		_putchar('\n')
+	else
+	{
+		for (int i = 0 ; i <= size ; ++i)
+		{
+			for (int j = 0 ; j < size - i ; ++j)
+				_putchar(' ');
+			for (int l = 0 ; l < i ; ++l)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
