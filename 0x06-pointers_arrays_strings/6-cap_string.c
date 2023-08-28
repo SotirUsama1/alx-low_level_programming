4c1457ecb2110ed88271672bb8bb39add6866b39
SotirUsama1 <sotirusama16@gmail.com>
2023-08-28 16:08:27 -0700

    task_6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @s: string variable
+ *
+ * Return: s variable
+*/
+char *cap_string(char *s)
+{
+	int i, j;
+	char sep[14] = {' ', '\t', '\n', ',', ';', '.', '!', '?', 34, '(', ')'
+		, '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] >= 97 && s[i] <= 122)
+			for (j = 0; j < 13; j++)
+				if (s[i - 1] == sep[j])
+					s[i] -= 32;
+	return (s);
+}
