e4ad4e65ec72ddce6b763469afc5797b246e7ab2
SotirUsama1 <sotirusama16@gmail.com>
2023-08-28 11:21:23 -0700

    task_3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,19 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-        int r, i = 0;
+	int r, i = 0;
 
-        while (s1[i] != '\0' && s2[i] != '\0')
-        {
-                if (s1[i] != s2[i])
-                {
-                        r = s1[i] - s2[i];
-                        break;
-                }
-                else
-                        r = s1[i] - s2[i];
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			r = s1[i] - s2[i];
+			break;
+		}
+		else
+			r = s1[i] - s2[i];
 
-                i++;
-        }
-        return (r);
+		i++;
+	}
+	return (r);
 }
