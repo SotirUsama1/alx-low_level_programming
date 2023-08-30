5fdc58b153d2e93ecdba012fb34be0bcd4c040c7
SotirUsama1 <sotirusama16@gmail.com>
2023-08-30 07:09:32 -0700

    task_7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,21 +1,22 @@
+/**
+ * leet - replace some letters with some numbers
+ *
+ * @s: string variable
+ * 
+ * Return: s
+*/
 char *leet(char *s)
 {
-        int i;
+	int i, j;
+	char lc[6] = {'a', 'e', 'o', 't', 'l'};
+	char uc[6] = {'A', 'E', 'O', 'T', 'L'};
+	char r[6] = {'4', '3', '0', '7', '1'};
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                if (s[i] == 'a' || s[i] == 'A')
-                        s[i] = '4';
-                else if (s[i] == 'e' || s[i] == 'E')
-                        s[i] = '3';
-                else if (s[i] == 'e' || s[i] == 'E')
-                        s[i] = '3';
-                else if (s[i] == 'o' || s[i] == 'O')
-                        s[i] = '0';
-                else if (s[i] == 't' || s[i] == 'T')
-                        s[i] = '7';
-                else if (s[i] == 'l' || s[i] == 'L')
-                        s[i] = '1';
-        }
-        return (s);
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == lc[j] || s[i] == uc[j])
+			        s[i] = r[j];
+		}
+	return (s);
 }
