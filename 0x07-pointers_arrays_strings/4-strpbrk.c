6188756648968e5eaafda50a4a0a2733c034e0d8
SotirUsama1 <sotirusama16@gmail.com>
2023-09-01 22:24:54 -0700

    task_4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,22 +9,26 @@
  * Return: pointer to the bytes in @s
  *         or NULL if no such byte is found
 */
-
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int i, j, flag;
 
-	i = 0;
-	while (s[i] != '\0')
+	for (i = 0; s[0] != '\0'; i++)
 	{
-		j = 0;
-		while (accept[j] != '\0')
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
-				return (s + i);
-			j++;
+			if (s[0] == accept[j])
+			{
+				flag = 1;
+				break;
+			}
 		}
-		i++;
+		if (flag == 1)
+		{
+			return (s);
+		}
+		s++;
 	}
-	return ('\0');
+	return (NULL);
 }
