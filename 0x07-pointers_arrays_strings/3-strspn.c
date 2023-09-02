9d2d63258d766130e990713201f28760506b872b
SotirUsama1 <sotirusama16@gmail.com>
2023-09-01 21:55:20 -0700

    task_3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,29 +7,28 @@
  *
  * Return: the number of bytes in the initial segment
 */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, f;
+	int i, j, f, flag;
 
-	i = 0;
-	while (s[i] != '\0')
+	f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		j = 0;
-		f = 1; /*flag status*/
-		while (accept[j] != '\0')
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				f = 0; /*success*/
-				break;
+				f++;
+				flag = 1;
 			}
-			j++;
 		}
-		if (f == 1)
-			break;
-		i++;
+		if (flag == 0)
+		{
+			return (f);
+		}
 	}
 
-	return (i);
+	return (0);
 }
