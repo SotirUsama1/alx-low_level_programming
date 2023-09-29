e6cf7162ec2fff82f3229a1dc74ed608409448a6
SotirUsama1 <sotirusama16@gmail.com>
2023-09-28 18:49:20 -0700

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion: the function
+ *
+ * @s: string
+*/
+
+void _puts_recursion(char *s);
+{
+	int i = 0;
+
+	if (s[i] != "\0")
+	{
+		_putchar(s[i]);
+		s++;
+		_puts_recursion(s);
+	}
+}
