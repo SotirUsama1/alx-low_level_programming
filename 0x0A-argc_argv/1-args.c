a5c6b031fb13135d4bef2476d3aa4a0fb22e2ee9
SotirUsama1 <sotirusama16@gmail.com>
2023-10-02 22:27:01 +0300

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - function prints number of arguments passed to the terminal
+ *
+ * @argc: arguments counter
+ * @argv: arguments array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void) argv;
+	return (0);
+}
