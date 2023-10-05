a2c5c1d775bfd698b50afd33ce8aaa3c0601e899
SotirUsama1 <sotirusama16@gmail.com>
2023-10-05 03:26:58 +0300

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicate a string
+ *
+ * @str: string variable
+ *
+ * Return: dup
+ */
+
+char *_strdup(char *str)
+{
+	int i, n;
+	char *dup;
+
+	for (n = 0; str[n] != '\0';)
+		n++;
+	if (n == 0)
+		return (NULL);
+	dup = (char *) malloc(sizeof(char) * n);
+
+	for (i = 0; i < n; i++)
+		dup[i] = str[i];
+	return (dup);
+}
