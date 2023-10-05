80328e461b993142cdc591c8eab409fde9665868
SotirUsama1 <sotirusama16@gmail.com>
2023-10-05 03:32:02 +0300

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,12 +15,11 @@ char *_strdup(char *str)
 
 	for (n = 0; str[n] != '\0';)
 		n++;
-	if (n == 0)
+	if (str == NULL)
 		return (NULL);
 	dup = (char *) malloc(sizeof(char) * n);
 
 	for (i = 0; i < n; i++)
 		dup[i] = str[i];
-	free(dup);
 	return (dup);
 }
