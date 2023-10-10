585673d3625f963f5832d58e169d9811737d93d7
SotirUsama1 <sotirusama16@gmail.com>
2023-10-10 03:17:23 +0300

    task101

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,7 @@
 #include <limits.h>
 #include <string.h>
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
