c59289dc6530791a4e029a94662ba584437fa8dc
SotirUsama1 <sotirusama16@gmail.com>
2023-10-11 04:14:37 +0300

    task3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#define ABS(X) ((x) > 0 ? (X) : (-X))
+#define ABS(X) ((x) > 0 ? (X) : (-(X)))
 
 #endif /* _FUNCTION_LIKE_MACRO_H */
