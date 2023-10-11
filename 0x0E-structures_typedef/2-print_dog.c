95249b0de0d48d90e68cfcc1b5bfe1004197ffc4
SotirUsama1 <sotirusama16@gmail.com>
2023-10-11 23:10:08 +0300

    task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * print_dog - print a variable of data tupe struct dog
+ *
+ * @d: pointer to variable address
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		(*d).name = "(nil)";
+	if (d->owner == NULL)
+                (*d).owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s", d-> name, d-> age, d-> owner);
+}
