0b8e539951969cf79b3f0523ef4016e7a51cd066
SotirUsama1 <sotirusama16@gmail.com>
2023-10-20 18:58:46 +0300

    task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a list
+ *
+ * @h: pointer to the head element of the list
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
