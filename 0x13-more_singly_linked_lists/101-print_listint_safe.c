c2866ee0e05688122713c8f90d828622064b0ae8
sotir <sotirusama16@gmail.com>
2024-08-15 04:12:35 +0300

    more singly linked lists fixes

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -14,7 +14,7 @@ size_t print_listint_safe(const listint_t *head)
 	tmp_n = head;
 	while (tmp_n)
 	{
-		printf("[%p] %d\n", (void*)tmp_n, tmp_n->n);
+		printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
 		counter++;
 		tmp_n = tmp_n->next;
 		l_n = head;
@@ -23,7 +23,7 @@ size_t print_listint_safe(const listint_t *head)
 		{
 			if (tmp_n == l_n)
 			{
-				printf("-> [%p] %d\n", (void*)tmp_n, tmp_n->n);
+				printf("-> [%p] %d\n", (void *)tmp_n, tmp_n->n);
 				return (counter);
 			}
 			l_n = l_n->next;
